2f633f10fb0c1d1417d8170218958d64a94333dc
DevUday <udayusq@gmail.com>
2020-09-11 21:47:36 +0600

    init git repo

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplaySpeed: 1000,
+    autoplayHoverPause: false
+
+})
